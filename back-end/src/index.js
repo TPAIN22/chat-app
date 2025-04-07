d638acd4fd5f79e06018f1e016f3085574217a01
TPAIN22 <mamyafreka@gmail.com>
2025-04-07 23:19:37 +0300

    last one

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -24,9 +24,10 @@ app.use("/api/message" , messageRoutes)
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname , '../front-end/dist')))
-    app.get('*' , (req , res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname , '../front-end/dist/index.html'))
-    })}
+    })
+}    
 server.listen(port, () => {
     console.log('Server is running on port' + port)
     connectDB()
