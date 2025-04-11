a5f7751841959611efe15c8c4bf588c0350d2378
TPAIN22 <mamyafreka@gmail.com>
2025-04-12 01:41:58 +0300

    one last time

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,9 +26,7 @@ app.use("/api/message", messageRoute)
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
-    })
+   
 }
 
 server.listen(PORT, () => {
