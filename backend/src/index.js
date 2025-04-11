8f475aff3b6fefd613c6ebd736c41e34e35b69ea
TPAIN22 <mamyafreka@gmail.com>
2025-04-12 01:27:48 +0300

    one last time

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,33 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import path from 'path'
-import { connect } from './lib/db.js'
-import authRoute from './routes/auth.route.js'
-import messageRoute from './routes/message.route.js'
-import { app , server} from './lib/socket.js'
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import path from "path"
+import { connect } from "./lib/db.js"
+import authRoute from "./routes/auth.route.js"
+import messageRoute from "./routes/message.route.js"
+import { app , server} from "./lib/socket.js"
 dotenv.config()
 const PORT = process.env.PORT
 const __dirname = path.resolve()
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser())
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true
 }))
 
 // Routes
-app.use('/api/auth', authRoute)
-app.use('/api/message', messageRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/message", messageRoute)
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'))
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
     })
 }
 
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import {protect} from '../middleware/auth.middleware.js'
-import { getUSersForSidebar  , getMessages , sendMessage} from '../controllers/message.controller.js'
+import express from "express"
+import {protect} from "../middleware/auth.middleware.js"
+import { getUSersForSidebar  , getMessages , sendMessage} from "../controllers/message.controller.js"
 const router = express.Router()
-router.get('/users' , protect , getUSersForSidebar)
-router.get('/:id' , protect , getMessages)
-router.post('/send/:id' , protect , sendMessage)
+router.get("/users" , protect , getUSersForSidebar)
+router.get("/:id" , protect , getMessages)
+router.post("/send/:id" , protect , sendMessage)
 export default router
