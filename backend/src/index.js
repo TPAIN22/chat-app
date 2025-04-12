8425bda2d92cbb4c7856b12c11628613ea9edf28
TPAIN22 <mamyafreka@gmail.com>
2025-04-12 08:35:27 +0300

    main

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,9 +28,9 @@ app.use("/api/message", messageRoute)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
   
-    app.get("/*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
+    app.get("(.*)", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+      });
   }
 server.listen(PORT, () => {
     console.log(`🚀 Server listening on port ${PORT}`)
