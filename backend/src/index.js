bfed98d9bbbda4e01ab4674a93036db480647f87
TPAIN22 <mamyafreka@gmail.com>
2025-04-12 08:30:24 +0300

    fr

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,18 +28,10 @@ app.use("/api/message", messageRoute)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
   
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
       res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
   }
-
-
-
-
-
-
-
-
 server.listen(PORT, () => {
     console.log(`🚀 Server listening on port ${PORT}`)
     connect()
